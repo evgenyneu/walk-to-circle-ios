b0b80331fee5467d1588061ca367e4d00e7d5fca
Evgenii Neumerzhitckii <evgeny@bikeexchange.com.au>
2014-09-14 12:24:11 +1000

    Prevent annotation from overlaping the button

--- walk to circleTests/ButtonOverlapTests.swift
+++ walk to circleTests/ButtonOverlapTests.swift
@@ -18,29 +18,51 @@ class ButtonOverlapTests: XCTestCase {
     let point = CGPoint(x: 114, y: 123)
     let delta = CGSize(width: 10, height: 10)
 
-    let result = obj.scollCorrection(delta, buttonRect: buttonRect, pinCoordinate: point)
+    let result = obj.scollCorrection(delta, buttonRect: buttonRect, pinCoordinate: point,
+      scrollToRightOnHorizontalCorrection: true)
 
     XCTAssertEqual(10, result.width)
     XCTAssertEqual(43, result.height)
   }
 
+  // scollCorrection
+  // --------------------------
+
   func testVerticalCorrection_correctVeritically() {
     let rect = CGRect(x: 100, y: 100, width: 50, height: 50)
     let point = CGPoint(x: 110, y: 130)
 
-    let result = obj.scollCorrection(rect, pinCoordinate: point)
+    let result = obj.scollCorrection(rect, pinCoordinate: point,
+      scrollToRightOnHorizontalCorrection: false)
+
     XCTAssertEqual(0, result.width)
     XCTAssertEqual(-50, result.height)
   }
 
-  func testVerticalCorrection_correctHorizontally() {
+  func testVerticalCorrection_correctHorizontally_left() {
     let rect = CGRect(x: 200, y: 200, width: 50, height: 50)
     let point = CGPoint(x: 210, y: 230)
 
-    let result = obj.scollCorrection(rect, pinCoordinate: point)
+    let result = obj.scollCorrection(rect, pinCoordinate: point,
+      scrollToRightOnHorizontalCorrection: false)
+
     XCTAssertEqual(-110, result.width)
     XCTAssertEqual(0, result.height)
   }
+
+  func testVerticalCorrection_correctHorizontally_right() {
+    let rect = CGRect(x: 200, y: 200, width: 50, height: 50)
+    let point = CGPoint(x: 210, y: 230)
+
+    let result = obj.scollCorrection(rect, pinCoordinate: point,
+      scrollToRightOnHorizontalCorrection: true)
+
+    XCTAssertEqual(140, result.width)
+    XCTAssertEqual(0, result.height)
+  }
+
+  // buttonOverlapsPin
+  // --------------------------
 
   func testButtonOverlapsPin_YES() {
     let rect = CGRect(x: 100, y: 100, width: 50, height: 50)
@@ -53,7 +75,7 @@ class ButtonOverlapTests: XCTestCase {
 
   func testButtonOverlapsPin_NO() {
     let rect = CGRect(x: 100, y: 100, width: 50, height: 50)
-    let point = CGPoint(x: 160, y: 130)
+    let point = CGPoint(x: 240, y: 170)
 
     let result = obj.buttonOverlapsPin(rect, pinCoordinate: point)
 
