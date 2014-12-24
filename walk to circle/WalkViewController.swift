57b18025be626e4b37f7e0971d1dec3405ef0a8f
Evgenii Neumerzhitckii <evgeny@bikeexchange.com.au>
2014-12-24 15:46:53 +1100

    Walk screen design

--- walk to circle/WalkViewController.swift
+++ walk to circle/WalkViewController.swift
@@ -10,9 +10,20 @@ import UIKit
 import CoreLocation
 
 class WalkViewController: UIViewController {
+  @IBOutlet weak var quoteLabel: UILabel!
+  @IBOutlet weak var authorLabel: UILabel!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
 
+    showBackground()
+  }
+  
+  @IBAction func onCancelTapped(sender: AnyObject) {
+    WalkViewControllers.Map.show()
+  }
+
+  private func showBackground() {
     let image = UIImage(named: "branch_background_1242_2208.png")
     let imageView = UIImageView(image: image)
     imageView.contentMode = UIViewContentMode.ScaleAspectFill
@@ -21,9 +32,5 @@ class WalkViewController: UIViewController {
 
     iiAutolayoutConstraints.fillParent(imageView, parentView: view, margin: 0, vertically: true)
     iiAutolayoutConstraints.fillParent(imageView, parentView: view, margin: 0, vertically: false)
-  }
-  
-  @IBAction func onCancelTapped(sender: AnyObject) {
-    WalkViewControllers.Map.show()
   }
 }
