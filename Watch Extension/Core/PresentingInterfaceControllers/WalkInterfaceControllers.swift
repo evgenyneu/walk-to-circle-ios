144188e93945a8fe53d7b112b6f8c5fe8971847d
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2015-12-15 15:07:52 +1100

    Remove watch

--- Watch Extension/Core/PresentingInterfaceControllers/WalkInterfaceControllers.swift
+++
@@ -1,9 +0,0 @@
-import WatchKit
-
-enum WalkInterfaceControllers: String {
-  case Walk
-  
-  func push(interfaceController: WKInterfaceController) {
-    interfaceController.pushControllerWithName(self.rawValue, context: nil)
-  }
-}
--- graphics/watch/buttons/Compass/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-./rotate.sh compass_arrow compass_arrows
-./pngcrushall.sh compass_arrows/
